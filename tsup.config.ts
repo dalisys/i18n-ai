0a2037dcca0816140c6a40e37d6b51e497abd49f
daly.devs@gmail.com <daly.devs@gmail.com>
2025-02-24 00:41:00 +0100

    Optimize build configuration and npm package publishing

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "tsup";
+import pkg from "./package.json";
 
 export default defineConfig(({ format }) => ({
   entry: ["src/index.ts", "src/cli.ts"],
   format: ["cjs", "esm"],
   dts: true,
   splitting: true,
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
   minify: true,
   treeshake: true,
   outDir: "dist",
   target: "node20",
+  external: Object.keys(pkg.dependencies || {}),
   banner: {
     js: format === "esm" ? "#!/usr/bin/env node" : "",
   },
